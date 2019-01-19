dbdd533e97185197f75e9757e650156b4332cc30
Matheus Cuba <matheusmcuba@gmail.com>
2019-01-19 10:41:56 -0200

    Setting Environment

---
+++ 1st1/gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename');
+
+const configs = {
+    less: {
+        watch: './src/less/*.less',
+        compile: './src/less/[^_]*.less',
+        output: './src/css/'
+    }
+};
+
+gulp.task('watch', function () {
+  gulp.watch(configs.less.watch, gulp.parallel('less'));
+});
+
+gulp.task('less', function () {
+
+  return gulp.src(configs.less.compile)
+    .pipe(less().on('error', function (err) {
+      console.log(err);
+    }))
+    .pipe(cssmin().on('error', function(err) {
+      console.log(err);
+    }))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(configs.less.output));
+
+});
+
+gulp.task('default', gulp.parallel('less', 'watch'));
